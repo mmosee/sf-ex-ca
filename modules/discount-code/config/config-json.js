d03d7653b7cbb9ecc7909f40322f60061d04fc99
mmosee <nammosee@gmail.com>
2024-04-29 09:03:54 +0900

    assd

--- modules/discount-code/config/config-json.js
+++ modules/discount-code/config/config-json.js
@@ -19,7 +19,8 @@ module.exports = function configJSON(req) {
         // See: https://developer.salesforce.com/docs/atlas.en-us.mc-apis.meta/mc-apis/how-data-binding-works.htm
         inArguments: [
           {
-            discount: 10
+            discount: 10,
+            test1: 'test1'
           }
         ],
         outArguments: [],
