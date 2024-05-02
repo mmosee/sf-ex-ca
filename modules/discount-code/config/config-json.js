767904313c651c872c23874029f81f1602463d9a
mmosee <nammosee@gmail.com>
2024-05-02 11:06:49 +0900

    execute outArg Phone

--- modules/discount-code/config/config-json.js
+++ modules/discount-code/config/config-json.js
@@ -31,7 +31,11 @@ module.exports = function configJSON(req) {
             Phone: "{{Contact.Attribute.testDE.Phone}}"
           }
         ],
-        outArguments: [],
+        outArguments: [
+          {
+            Phone: "{{Contact.Attribute.testDE.Phone}}"
+          }
+        ],
         // Fill in the host with the host that this is running on.
         // It must run under HTTPS
         url: `https://${req.headers.host}/modules/discount-code/execute`,
